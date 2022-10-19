21967444fa6ce06c12c509b3dff59b3f155fd596
JakhongirRasulov007 <jakhongirrasulov007@gmail.com>
2022-10-19 16:52:32 +0500

    changes access modifiers in Utils methods from private to public

--- src/main/java/com/example/sqbtask/utils/Utils.java
+++ src/main/java/com/example/sqbtask/utils/Utils.java
@@ -9,11 +9,11 @@ public  class Utils {
     private static final String regexPatternForEPP = "^\\+[0-9]{1,3}\\.[0-9]{4,14}(?:x.+)?$"; // regex for international phone number checking
     private static final Pattern patternForEPP = Pattern.compile(regexPatternForEPP);
 
-    private static boolean isPhoneCorrect(String number) {
+    public static boolean isPhoneCorrect(String number) {
         return patternForEPP.matcher(number).matches();
     }
 
-    private static boolean isWalletNumberCorrect(String walletNumber) {
+    public static boolean isWalletNumberCorrect(String walletNumber) {
         // walletNumber: XY where X = 999 and Y = 13 digits
         if (walletNumber.length() != 16) return false;
         for (int i = 0; i < walletNumber.length(); i++) {
@@ -27,5 +27,4 @@ public  class Utils {
         }
         return true;
     }
-
 }
