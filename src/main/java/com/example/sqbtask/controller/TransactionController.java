1be43def093d4cb64069f67c851eb7e012e5075c
JakhongirRasulov007 <jakhongirrasulov007@gmail.com>
2022-10-20 19:29:57 +0500

    adds endpoint urls to TransactionController

--- src/main/java/com/example/sqbtask/controller/TransactionController.java
+++ src/main/java/com/example/sqbtask/controller/TransactionController.java
@@ -4,8 +4,11 @@ import com.example.sqbtask.datatype.*;
 import com.example.sqbtask.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.plaf.PanelUI;
 
 
 @RequiredArgsConstructor
@@ -15,15 +18,17 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
+    @PostMapping(TransactionEndpoints.PERFORM_TRANSACTION)
     public PerformTransactionResult performTransaction(PerformTransactionArgument performTransactionArgument) {
         return transactionService.performTransaction(performTransactionArgument);
     }
 
-    //TODO: not enough description for input and output
+    @PostMapping(TransactionEndpoints.CHECK_TRANSACTION)
     public CheckTransactionResponse checkTransaction(CheckTransactionRequest checkTransactionRequest) {
         return transactionService.checkTransaction(checkTransactionRequest);
     }
 
+    @PostMapping(TransactionEndpoints.CANCEL_TRANSACTION)
     public CancelTransactionResponse cancelTransaction(CancelTransactionRequest cancelTransactionRequest) {
         return transactionService.cancelTransaction(cancelTransactionRequest);
     }
@@ -31,5 +36,8 @@ public class TransactionController {
     @UtilityClass
     public static class TransactionEndpoints {
         public static final String BASE_URL = "/api/v1/transaction";
+        public static final String PERFORM_TRANSACTION = "";
+        public static final String CHECK_TRANSACTION = "/check";
+        public static final String CANCEL_TRANSACTION = "/cancel";
     }
 }
